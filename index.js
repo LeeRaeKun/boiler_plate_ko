3c58ab488c5f751c3b6e0ee73c96f58052cd52a7
atozjames <atozjames@gmail.com>
2020-11-25 17:33:59 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express()
 const port = 5000
 
 const mongoose = require('mongoose')
+const { User } = require("./models/User");
+const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://rgtwins:lgsj0224@boilerplate.m2jyy.mongodb.net/<dbname>?retryWrites=true&w=majority', { 
+const config= require('./config/key');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURI, { 
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connect ....'))
   .catch(err => console.log(err))
@@ -13,6 +23,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!~~이래근')
 })
 
+app.post('/register', (req, res) => {
+  // 회원가입할때 필요한 정보들을 Client에서 가져오면 
+  // 그것들을 데이터베이스에 넣어준다
+  
+  const user = new User(req.body)
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true})
+  }) ;
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
